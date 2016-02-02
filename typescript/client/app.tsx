ace72ba2db863952fd477cf69aa85f485d05d261
Ali SHeehan-Dare <alisd23@live.co.uk>
2016-02-02 17:06:57 +0000

    Fixed react-router issue (upgraded to their master)

--- typescript/client/app.tsx
+++ typescript/client/app.tsx
@@ -2,16 +2,16 @@ import * as React from 'react';
 import { render } from 'react-dom';
 
 import { Provider } from 'react-redux';
-import { Router } from 'react-router';
+import { Router, browserHistory } from 'react-router';
 import LogMonitor from 'redux-devtools-log-monitor';
 import DockMonitor from 'redux-devtools-dock-monitor';
 import { createDevTools } from 'redux-devtools';
 
 import ReducerRegistry from './redux/ReducerRegistry';
-import { browserHistory } from 'react-router';
 
 import configureRoutes from './configureRoutes';
 import { configureClient } from './configureStore';
+import { match } from 'react-router';
 
 import coreReducers from './redux/core';
 import productPage from './redux/modules/productPage';
@@ -29,21 +29,25 @@ const DevTools = createDevTools(
 )
 
 const initialState = (window as any).__INITIAL_STATE__;
-console.log(initialState);
 const routes = configureRoutes(reducerRegistry);
 const store = configureClient(reducerRegistry, DevTools, initialState);
+console.log(initialState);
+console.log(browserHistory);
 
-render(
-  <Provider store={store}>
-    <div>
-      <Router history={browserHistory} routes={routes} />
-    </div>
-  </Provider>,
-  document.getElementById('root')
-)
+match({ history: browserHistory, routes } as any, (error, redirectLocation, renderProps) => {
+  render(
+    <Provider store={store}>
+      <div>
+        <Router {...renderProps} />
+      </div>
+    </Provider>,
+    document.getElementById('root')
+  );
+});
+
 
 // Render dev tools
-render(
-  <DevTools store={store} />,
-  document.getElementById('dev-tools')
-)
+// render(
+//   <DevTools store={store} />,
+//   document.getElementById('dev-tools')
+// )
